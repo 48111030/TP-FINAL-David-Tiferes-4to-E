722cb927ab3c740470422d463d0013d684dcf526
48111030 <48111030@est.ort.edu.ar>
2024-06-19 15:22:05 -0300

    aesthetic changes and code finished

--- Assets/ObjectAppearenceGenerator.cs
+++ Assets/ObjectAppearenceGenerator.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class ObjectAppearenceGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] cubes;
+    public int currentIndex = 0;
+
     void Start()
     {
-        
+        DeactivateAll();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+       
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentIndex++;
+            if (currentIndex < cubes.Length)
+            {
+                DeactivateAll();
+                ActibateByIndex(currentIndex);
+
+            }
+            else if (currentIndex > cubes.Length)
+            {
+                currentIndex = -1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            DeactivateAll();
+            ActibateByIndex(currentIndex);
+            currentIndex--;
+            if (currentIndex < 0)
+            {
+                currentIndex = cubes.Length - 1;
+            }
+
+
+        }
     }
+
+    void DeactivateAll()
+    {
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            cubes[i].SetActive(false);
+        }
+    }
+    void ActivateAll()
+    {
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            cubes[i].SetActive(true);
+        }
+    }
+    void ActibateByIndex(int index)
+    {
+        cubes[index].SetActive(true);
+    }
+
 }
